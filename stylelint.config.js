ce5d65b196a14941348f5829eec0602ba042abfe
toono-f <52028799+toono-f@users.noreply.github.com>
2023-06-03 10:45:03 +0900

    初回環境設定

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugin: ["stylelint-scss"],
+  extends: [
+    "stylelint-config-recommended-scss", // scssのための拡張ルール追加
+    "stylelint-config-recess-order", // cssプロパティの自動ソートを設定
+    "stylelint-config-prettier", // Prettierとの競合ルールをOFFにする
+  ],
+  rules: {
+    "scss/at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
+    ],
+  },
+};
