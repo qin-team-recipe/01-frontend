d5f3d8778400326351db5c45b18ea2bb783e9b79
TAKO-zhangyu <takoforme@gmail.com>
2023-06-17 15:30:02 +0900

    feat: いいねカウントUIを実装

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,16 +1,25 @@
 import styles from './Recipe.module.scss';
 import Image from "next/image";
 
+
 export const Recipe = () => {
+  const fav_count_number = 1234
   return (
     <div className="inner">
-      <Image
-        src="/images/recipe-dummy.png"
-        alt=""
-        width={173}
-        height={173}
-        className={styles.img}
-      />
+      <div className={styles.imgWrapper}>
+        <div className={styles.favCount}>
+        <Image src="/icon/heart.svg" width={11.67} height={10.5} alt="" />
+          <span className={styles.favNumber}>{fav_count_number.toLocaleString()}</span>
+        </div>
+
+        <Image
+          src="/images/recipe-dummy.png"
+          alt=""
+          width={173}
+          height={173}
+          className={styles.img}
+        />
+      </div>
 
       <h1 className={styles["recipe-name"]}>トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン</h1>
       <p className={styles["recipe-description"]}>
