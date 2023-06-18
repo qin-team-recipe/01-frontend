8442d3b6ca1cbfaaffde5a615d473f23938690b3
TAKO-zhangyu <takoforme@gmail.com>
2023-06-18 22:35:59 +0900

    yarn format実行

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,15 +1,17 @@
-import styles from './Recipe.module.scss';
 import Image from "next/image";
 
+import styles from "./Recipe.module.scss";
 
 export const Recipe = () => {
-  const fav_count_number = 1234
+  const fav_count_number = 1234;
   return (
     <div className="inner">
       <div className={styles.imgWrapper}>
         <div className={styles.favCount}>
-        <Image src="/icon/heart.svg" width={11.67} height={10.5} alt="" />
-          <span className={styles.favNumber}>{fav_count_number.toLocaleString()}</span>
+          <Image src="/icon/heart.svg" width={11.67} height={10.5} alt="" />
+          <span className={styles.favNumber}>
+            {fav_count_number.toLocaleString()}
+          </span>
         </div>
 
         <Image
@@ -21,7 +23,9 @@ export const Recipe = () => {
         />
       </div>
 
-      <h1 className={styles["recipe-name"]}>トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン</h1>
+      <h1 className={styles["recipe-name"]}>
+        トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン
+      </h1>
       <p className={styles["recipe-description"]}>
         ウルトラハイパー超すごいしまぶーシェフ
       </p>
