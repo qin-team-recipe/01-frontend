cab4b97f0b16cecceb3c26a8ed9a02ae74b56e59
TAKO-zhangyu <takoforme@gmail.com>
2023-06-22 00:23:59 +0900

    mod: storybookを今風にした

--- src/components/RecipeCard/RecipeCard.stories.tsx
+++ src/components/RecipeCard/RecipeCard.stories.tsx
@@ -1,18 +1,13 @@
-import { Meta, StoryFn } from "@storybook/react";
-import React from "react";
-
-import { RecipeCard, RecipeCardProps } from "./RecipeCard";
+import RecipeCard from "./RecipeCard";
 
 export default {
-  title: "Components/RecipeCard",
   component: RecipeCard,
-} as Meta;
+};
 
-const Template: StoryFn<RecipeCardProps> = (args) => <RecipeCard {...args} />;
-
-export const Default: StoryFn<RecipeCardProps> = Template.bind({});
-Default.args = {
-  favCountNumber: 12345,
-  name: "Delicious Recipe",
-  description: "This is a description of a delicious recipe.",
+export const Default = {
+  args: {
+    favCountNumber: 12345,
+    name: "Delicious Recipe",
+    description: "This is a description of a delicious recipe.",
+  },
 };
