f0c3656af3a8da0a648b555a31400860718343e1
choimaru <choimaru8@gmail.com>
2023-06-24 18:01:57 +0900

    シェフ一覧のカード

--- src/components/ChefCard/ChefCard.stories.tsx
+++ src/components/ChefCard/ChefCard.stories.tsx
@@ -7,8 +7,9 @@ export default {
 export const Default = {
   args: {
     src: "/images/featured-chef1.png",
-    width: 148,
-    height: 220,
     name: "山田 Chef",
+    description:
+      "初の絵本出版！『まねっこシェフ』・ふわふわ！スクランブルエッグ・にぎにぎ！おにぎり・主婦の友社より３月３日、２冊同時発売！絶賛発売中！",
+    count: 123,
   },
 };
--- src/components/ChefCard/ChefCard.tsx
+++ src/components/ChefCard/ChefCard.tsx
@@ -4,21 +4,30 @@ import styles from "./ChefCard.module.scss";
 
 export interface ChefCardProps {
   src: string;
-  width: number;
-  height: number;
-  name?: string;
+  name: string;
+  description: string;
+  count: number;
 }
 
 export const ChefCard: React.FC<ChefCardProps> = ({
   src,
-  width,
-  height,
   name,
+  description,
+  count = 0,
 }) => {
   return (
-    <div className={styles["chef-image"]}>
-      <Image src={src} width={width} height={height} alt="" />
-      <p className={styles["chef-name"]}>{name}</p>
+    <div className={styles["chef-card"]}>
+      <div className={styles["card-left"]}>
+        <Image src={src} width={88} height={116} alt="" />
+      </div>
+      <div className={styles["card-right"]}>
+        <div className={styles["chef-name"]}>{name}</div>
+        <div className={styles["chef-description"]}>{description}</div>
+        <div className={styles["recipe-count"]}>
+          <Image src={"/icon/knife-fork.svg"} width={16} height={16} alt="" />
+          <p>{count}レシピ</p>
+        </div>
+      </div>
     </div>
   );
 };
