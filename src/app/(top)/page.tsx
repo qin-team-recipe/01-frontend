16d43b5c9a0ca273fa0567bc4d6158ca35ad13e5
choimaru <choimaru8@gmail.com>
2023-06-24 14:14:26 +0900

    レイアウト修正

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -1,11 +1,28 @@
-import Image from "next/image";
+// import { ChefCard } from "@/components/ChefCard";
 
 import styles from "./top.module.scss";
 
 const dummyChefList = [
-  { name: "山田シェフ1" },
-  { name: "山田シェフ2" },
-  { name: "山田シェフ3" },
+  {
+    id: 3,
+    name: "アンミカ",
+    thumbnail: "/images/featured-chef1.png",
+  },
+  {
+    id: 4,
+    name: "ジュリア",
+    thumbnail: "/images/featured-chef2.png",
+  },
+  {
+    id: 5,
+    name: "トム",
+    thumbnail: "/images/featured-chef3.png",
+  },
+  {
+    id: 6,
+    name: "しまぶー",
+    thumbnail: "/images/featured-chef4.png",
+  },
 ];
 
 export default function Page() {
@@ -16,22 +33,16 @@ export default function Page() {
         <div className={styles["featured-chef"]}>
           {dummyChefList.map((chef, index) => (
             <div className={styles["chef-item"]} key={index}>
-              <div className={styles["chef-image"]}>
-                <Image
-                  src={"/images/chef-dummy.png"}
-                  width={160}
-                  height={208}
-                  alt=""
-                />
-              </div>
-              <p className={styles["chef-name"]}>{chef.name}</p>
+              {/* <ChefCard src={chef.thumbnail} name={chef.name} /> */}
             </div>
           ))}
         </div>
       </section>
       <section className={styles.section}>
         <h2 className={styles.title}>話題のレシピ</h2>
-        <div className={styles["topic-recipe"]}></div>
+        <div className={styles["topic-recipe"]}>
+          {/* <RecipeCard favCountNumber={1234} name="ddddd" description="aaaa" /> */}
+        </div>
       </section>
       <section className={styles.section}>
         <h2 className={styles.title}>シェフ</h2>
