eb0e3b9b0375ad5a19d8189a5412c4bd38c3f7b3
kamiji-dai <kohuizhidao995@gmail.com>
2023-07-09 09:43:32 +0900

    タブコンポーネント作成

---
+++ src/components/TabBar/TabBar.stories.tsx
@@ -0,0 +1,15 @@
+import { TabBar } from "./TabBar";
+
+export default {
+  component: TabBar,
+};
+
+export const Default = {
+  args: {
+    data: [
+      { id: 1, label: "Tab 1" },
+      { id: 2, label: "Tab 2" },
+      { id: 3, label: "Tab 3" },
+    ],
+  },
+};
