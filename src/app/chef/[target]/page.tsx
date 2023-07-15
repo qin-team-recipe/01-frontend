7437db3f0f4932779132df68f667eb593eb0e124
kamiji-dai <kohuizhidao995@gmail.com>
2023-07-15 20:51:02 +0900

    fix: 新着レシピ/人気レシピに修正

--- src/app/chef/[target]/page.tsx
+++ src/app/chef/[target]/page.tsx
@@ -4,8 +4,8 @@ import styles from "./chef.module.scss";
 
 // TODO: APIと連携するまでの仮実装
 const dummyData = [
-  { id: 1, label: "レシピ" },
-  { id: 2, label: "リンク" },
+  { id: 1, label: "新着レシピ" },
+  { id: 2, label: "人気レシピ" },
 ];
 
 export default function Page({ params }: { params: { target: string } }) {
