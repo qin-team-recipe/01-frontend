0ad067bb6b87d161f57be8ff161c388dc70a8605
choimaru <choimaru8@gmail.com>
2023-07-15 15:53:38 +0900

    検索タイトル

--- src/app/(top)/search/[target]/page.tsx
+++ src/app/(top)/search/[target]/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import styles from "./search-target.module.scss";
+
 export default function Page({ params }: { params: { target: string } }) {
-  return <div>search/page/{params.target}</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const queryWord = searchParams.get("q");
+  const path = usePathname();
+  const isRecipe = path.startsWith("/search/recipe");
+  let searchTitle = "";
+  if (queryWord) {
+    const decode = decodeURIComponent(queryWord);
+    searchTitle = `「${decode}」で検索`;
+  } else {
+    searchTitle = isRecipe ? "話題のレシピ" : "シェフ一覧";
+  }
+  return <div className={styles["result-title"]}>{searchTitle}</div>;
 }
