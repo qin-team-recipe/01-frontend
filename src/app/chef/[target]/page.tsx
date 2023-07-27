05f291d75e623f0053a00c88f428da3e93f0fd68
kamiji-dai <kohuizhidao995@gmail.com>
2023-07-28 00:02:02 +0900

    followbuttonコンポーネント作成

--- src/app/chef/[target]/page.tsx
+++ src/app/chef/[target]/page.tsx
@@ -1,3 +1,4 @@
+import { FollowButton } from "@/components/FollowButton";
 import { TabBar } from "@/components/TabBar";
 
 import styles from "./chef.module.scss";
@@ -7,11 +8,21 @@ const dummyData = [
   { id: 1, label: "新着レシピ" },
   { id: 2, label: "人気レシピ" },
 ];
+const dummyData2 = {
+  initialText: "フォローする",
+  followingText: "フォロー中",
+  isFollowing: false,
+};
 
 export default function Page({ params }: { params: { target: string } }) {
   return (
     <main className={styles.page}>
       <div>chef/{params.target}</div>
+      <FollowButton
+        initialText={dummyData2.initialText}
+        followingText={dummyData2.followingText}
+        isFollowing={dummyData2.isFollowing}
+      />
       <TabBar data={dummyData} />
     </main>
   );
