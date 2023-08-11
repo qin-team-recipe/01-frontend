71734e9a9e1fa1332eb88cea9e276b89ca7a42c3
pomyk9 <eveningsong0253@gmail.com>
2023-08-11 11:23:25 +0900

    fix Invalid DOM property

--- src/components/Icon/IconArrowBack.tsx
+++ src/components/Icon/IconArrowBack.tsx
@@ -6,9 +6,9 @@ export const IconArrowBack = ({ color }: { color: string }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke={color}
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="h-5 w-5"
     aria-hidden="true"
   >
