7cd01f9a451eed923c325455046c95e4bc492120
choimaru <94576178+choimaru@users.noreply.github.com>
2023-09-02 16:57:23 +0900

    お気に入りシェフのコンポーネント化

---
+++ src/components/ChefCircleImage/ChefCircleImage.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import styles from "./ChefCircleImage.module.scss";
+
+export type ChefCircleImageProps = {
+  id: string;
+  thumbnail: string;
+  name?: string;
+};
+
+export const ChefCircleImage: React.FC<ChefCircleImageProps> = ({
+  id,
+  thumbnail,
+  name,
+}) => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles["chef-circle"]}>
+        <Link href={`/chef/${id}`}>
+          <Image src={thumbnail} width={68} height={68} alt="" />
+        </Link>
+      </div>
+      <div className={styles["chef-name"]}>{name}</div>
+    </div>
+  );
+};
---
+++ src/components/ChefCircleImage/index.tsx
@@ -0,0 +1,1 @@
+export { ChefCircleImage } from "@/components/ChefCircleImage/ChefCircleImage";
